3d62be748e590cf9a863352b84415c972112b710
super0605 <don.zhang0605@gmail.com>
2025-07-23 12:18:50 -0700

    feat: Add browser navigation logic

--- visual-automation-summarization/agentic_brwoser.py
+++ visual-automation-summarization/agentic_brwoser.py
@@ -13,3 +13,36 @@ from playwright.async_api import async_playwright, Page, Browser
 
 
 load_dotenv()
+
+#another way of implementing browser: Browser | None
+browser = Union[Browser, None] 
+page = Union[Page, None]
+
+
+#defining the state dictionary which will be passed between nodes
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages] #for conversation history
+    url: Union[str, None]
+    current_ss: Union[List[str], None]
+    summaries: Annotated[Sequence[BaseMessage], add_messages]
+    scroll_decision: Union[str, None]
+    task: str
+
+
+async def initialize_browser():
+    """
+    Initialize the playwright browser and page
+    """
+
+    global browser, page
+    print('-----Initializing the Playwright browser-----')
+
+    try:
+        pw = await async_playwright().start()
+        browser = await pw.chromium.launch(headless = False)
+
+        page = await browser.new_page()
+        print('-----Browser Initialized-----')
+
+    except Exception as e:
+        print(f'Failed to initialized browser due the following exception: {e}')
